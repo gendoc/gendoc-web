d9eb8f5b5fa69a7bc102aeb7b8d2131a14650ad1
lsjpjs1 <lsjpjs1@naver.com>
2023-04-03 22:51:18 +0900

    feat: GoogleDrivePicker 추가

---
+++ src/pages/GoogleDrivePicker.js
@@ -0,0 +1,37 @@
+import  { useEffect } from 'react';
+import useDrivePicker from 'react-google-drive-picker'
+
+
+const GoogleDrivePicker = () => {
+    const [openPicker, authResponse] = useDrivePicker();
+    // const customViewsArray = [new google.picker.DocsView()]; // custom view
+    const handleOpenPicker = () => {
+        openPicker({
+            clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+            developerKey: process.env.REACT_APP_GOOGLE_DEVELOPER_KEY,
+            viewId: "DOCUMENTS",
+            // token: token, // pass oauth token in case you already have one
+            showUploadView: true,
+            showUploadFolders: true,
+            supportDrives: true,
+            multiselect: true,
+            // customViews: customViewsArray, // custom view
+            callbackFunction: (data) => {
+                if (data.action === 'cancel') {
+                    console.log('User clicked cancel/close button')
+                }
+                console.log(data)
+            },
+        })
+    }
+
+
+
+    return (
+        <div>
+            <button onClick={() => handleOpenPicker()}>Open Picker</button>
+        </div>
+    );
+}
+
+export default GoogleDrivePicker;
