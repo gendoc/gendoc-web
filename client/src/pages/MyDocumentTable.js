bf56ed26c14e66a907342e5326acee8a6f4f7893
lsjpjs1 <lsjpjs1@naver.com>
2023-04-06 00:08:32 +0900

    feat: 업로드 문서 확인 기능 추가

---
+++ client/src/pages/MyDocumentTable.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import moment from "moment";
+
+function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function MyDocumentTable(props) {
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>파일명</TableCell>
+                        <TableCell align="right">작업상태</TableCell>
+                        <TableCell align="right">업로드 시간</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.documents.map((document) => (
+                        <TableRow
+                            key={document.fileName}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {document.fileName}
+                            </TableCell>
+                            <TableCell align="right">{"작업중"}</TableCell>
+                            <TableCell align="right">{moment(document.uploadTime, 'YYYYMMDDHHmmss z').add(9, "h").fromNow()}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
