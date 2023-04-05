435270bbd796c732498127089379077634473788
lsjpjs1 <lsjpjs1@naver.com>
2023-04-05 16:07:42 +0900

    feat: 세션 설정 추가

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function(req, res, next) {
 
-
+  console.log(req.sessionID)
   res.render('index', { title: 'Express' });
 });
 
