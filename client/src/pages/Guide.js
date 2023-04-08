ef78487edac15c58d547a73e43adab5d2862ec94
lsjpjs1 <lsjpjs1@naver.com>
2023-04-08 17:32:43 +0900

    feat: 가이드 문구 추가

--- client/src/pages/Guide.js
+++ client/src/pages/Guide.js
@@ -33,28 +33,67 @@ export default function Guide(state) {
                             }}
                             style={{background:"#ECE9E9"}}
                         >
-                            {["새 프로젝트를 눌러 가이드 문서와 사업계획서 초안을 업로드해주세요. \n(만약, 전문가 작성 가이드가 없으시다면 genDOC에서 기본 제공하는 전문가 작성 가이드를 단운 받아 업로드 해주세요.)",
-                                "새 프로젝트를 눌러 가이드 문서와 사업계획서 초안을 업로드해주세요. (만약, 전문가 작성 가이드가 없으시다면 genDOC에서 기본 제공하는 전문가 작성 가이드를 단운 받아 업로드 해주세요.)",
-                                "새 프로젝트를 눌러 가이드 문서와 사업계획서 초안을 업로드해주세요. (만약, 전문가 작성 가이드가 없으시다면 genDOC에서 기본 제공하는 전문가 작성 가이드를 단운 받아 업로드 해주세요.)"].map((elevation) => (
-                                        <Item key={Math.floor(Math.random() * 10000)} style={{height:"491px",borderRadius:"17px",paddingBlock:"37px",paddingInline:"20px",textAlign:"left"}} >
+                            <Item key={Math.floor(Math.random() * 10000)} style={{height:"491px",borderRadius:"17px",paddingBlock:"37px",paddingInline:"20px",textAlign:"left"}} >
 
-                                            <Typography
-                                                sx={{ flex: '1 1 100%' }}
-                                                variant="h4"
-                                                id="tableTitle"
-                                                component="div"
-                                                marginBottom={"30px"}
-                                            >
-                                                STEP 1.
-                                            </Typography>
-                                            <hr style={{marginBottom:"20px"}}/>
-                                            <Typography style={{whiteSpace: 'pre-wrap'}}>
-                                                {elevation}
-                                            </Typography>
+                                <Typography
+                                    sx={{ flex: '1 1 100%' }}
+                                    variant="h4"
+                                    id="tableTitle"
+                                    component="div"
+                                    marginBottom={"30px"}
+                                >
+                                    STEP 1_문서 업로드
+                                </Typography>
+                                <hr style={{marginBottom:"20px"}}/>
+                                <Typography style={{whiteSpace: 'pre-wrap'}}>
+                                    {" 좌측에 ‘새 프로젝트’를 눌러 ‘모집 공고문 문서’,’전문가 작성 가이드' 와 ‘사업계획서 초안’을 업로드 후 ‘생성하기’를 눌러주세요. 🎯\n" +
+                                        "\n" +
+                                        " ( ‘전문가 작성 가이드’가 없다면 좌측 ‘전문가 작성 가이드' 탭을 클릭하여 genDOC에서 기본 제공하는 전문가 작성 가이드를 내려받아, 업로드해 주세요. )"}
+                                </Typography>
 
-                                        </Item>
+                            </Item>
 
-                            ))}
+                            <Item key={Math.floor(Math.random() * 10000)} style={{height:"491px",borderRadius:"17px",paddingBlock:"37px",paddingInline:"20px",textAlign:"left"}} >
+
+                                <Typography
+                                    sx={{ flex: '1 1 100%' }}
+                                    variant="h4"
+                                    id="tableTitle"
+                                    component="div"
+                                    marginBottom={"30px"}
+                                >
+                                    STEP 2_AI 챗봇 활용
+                                </Typography>
+                                <hr style={{marginBottom:"20px"}}/>
+                                <Typography style={{whiteSpace: 'pre-wrap'}}>
+                                    {" 업로드한 ‘공고문 및 가이드 문서’를 기반으로 작성 문서 초안이 첨삭 완료되기까지, 대기 시간이 생깁니다.  대기 시간에 생성 폴더 항목에 존재하는 ‘genDOC 캐릭터’를 클릭하여, 공고 가이드 및 전문가 작성법 내용 기반에 정보를 물어보고 완성도 높은 문서를 만들어보세요. 📑 🤖"}
+                                </Typography>
+
+                            </Item>
+
+                            <Item key={Math.floor(Math.random() * 10000)} style={{height:"491px",borderRadius:"17px",paddingBlock:"37px",paddingInline:"20px",textAlign:"left"}} >
+
+                                <Typography
+                                    sx={{ flex: '1 1 100%' }}
+                                    variant="h4"
+                                    id="tableTitle"
+                                    component="div"
+                                    marginBottom={"30px"}
+                                >
+                                    STEP 3_첨삭 내용 확인
+                                </Typography>
+                                <hr style={{marginBottom:"20px"}}/>
+                                <Typography style={{whiteSpace: 'pre-wrap'}}>
+                                    {" 생성된 프로젝트의 상태가 ‘첨삭 완료’로 바뀌면, 해당 폴더를 접속하여, ‘첨삭 문서’를 열람하면, 기존 문서 초안 내용과 첨삭된 내용을 대조 확인할 수 있어요. 단기간에 완성도 높은 문서 작업을 경험해 보세요-! 🥳\n" +
+                                        "\n" +
+                                        " ( 프로젝트 폴더에서 업로드한 ‘공고문 및 가이드 문서'를 확인할 수 있어요. )"}
+                                </Typography>
+
+                            </Item>
+
+
+
+
                         </Box>
                 </Grid>
 
