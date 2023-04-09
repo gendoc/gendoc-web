acd3d6dc9f6d723689b0b6904e0ab6bd38140969
lsjpjs1 <lsjpjs1@naver.com>
2023-04-09 19:37:09 +0900

    feat: 전문가 작성 가이드 파일 수정

--- client/src/components/nav-section/NavSection.js
+++ client/src/components/nav-section/NavSection.js
@@ -34,14 +34,15 @@ function NavItem({ item }) {
 
   const handleClick = () =>{
       if (title=='전문가 작성 가이드') {
-          window.open("https://gendoc-prod.s3.ap-northeast-2.amazonaws.com/%E1%84%8F%E1%85%A5%E1%86%AB%E1%84%89%E1%85%A5%E1%86%AF%E1%84%90%E1%85%A5%E1%86%AB%E1%84%90%E1%85%B3+%E1%84%8C%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A5%E1%86%BC%E1%84%87%E1%85%A5%E1%86%B8+%E1%84%86%E1%85%A6%E1%84%82%E1%85%B2%E1%84%8B%E1%85%A5%E1%86%AF+%E1%84%80%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3+1%E1%84%87%E1%85%AE.hwp", '_blank');
-          window.open("https://gendoc-prod.s3.ap-northeast-2.amazonaws.com/%E1%84%8F%E1%85%A5%E1%86%AB%E1%84%89%E1%85%A5%E1%86%AF%E1%84%90%E1%85%A5%E1%86%AB%E1%84%90%E1%85%B3+%E1%84%8C%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A5%E1%86%BC%E1%84%87%E1%85%A5%E1%86%B8+%E1%84%86%E1%85%A6%E1%84%82%E1%85%B2%E1%84%8B%E1%85%A5%E1%86%AF+%E1%84%80%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3+1%E1%84%87%E1%85%AE.pdf", '_blank');
+          window.open("https://gendoc-prod.s3.ap-northeast-2.amazonaws.com/%E1%84%8F%E1%85%A5%E1%86%AB%E1%84%89%E1%85%A5%E1%86%AF%E1%84%90%E1%85%A5%E1%86%AB%E1%84%90%E1%85%B3+%E1%84%8C%E1%85%A1%E1%86%A8%E1%84%89%E1%85%A5%E1%86%BC%E1%84%87%E1%85%A5%E1%86%B8+%E1%84%86%E1%85%A6%E1%84%82%E1%85%B2%E1%84%8B%E1%85%A5%E1%86%AF+%E1%84%80%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%83%E1%85%B3.zip", '_blank');
       } else if (title=="내 드라이브"){
 
       } else {
           alert("Coming soon!")
       }
   }
+
+
 
   return (
     <StyledNavItem
