740088a6a7d9e3b7d77a63dc7fb6518383f0ceac
lsjpjs1 <lsjpjs1@naver.com>
2023-04-09 20:12:49 +0900

    feat: 챗봇 아이콘 추가

--- client/src/pages/MyProjectTable.js
+++ client/src/pages/MyProjectTable.js
@@ -12,6 +12,7 @@ import newScript from "../utils/scriptReader";
 import {gapiLoaded, gisLoaded} from "./GapiClient";
 import {callGetGuideDocuments, callGetWrittenDocuments} from "../modules/document";
 import {Link} from "react-router-dom";
+import {Tooltip} from "@mui/material";
 
 function createData(name, calories, fat, carbs, protein) {
     return {name, calories, fat, carbs, protein};
@@ -26,7 +27,46 @@ const rows = [
 ];
 
 export default function MyProjectTable(props) {
+    const [open, setOpen] = React.useState(false);
 
+    useEffect(() => {
+        handleTooltipOpen()
+        const timer = setTimeout(() => {
+            handleTooltipClose()
+            // Your callback function logic here
+        }, 5000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
+    const handleTooltipClose = () => {
+        setOpen(false);
+    };
+
+    const handleTooltipOpen = () => {
+        setOpen(true);
+    };
+
+    const CustomToolTip = (props) => {
+        return (
+    <Tooltip
+        PopperProps={{
+            disablePortal: true,
+        }}
+        onClose={handleTooltipClose}
+        open={open}
+        onMouseOver={handleTooltipOpen}
+        // disableFocusListener
+        // disableHoverListener
+        // disableTouchListener
+        title="첨삭이 완성될 때까지 해당 캐릭터를 눌러 AI챗봇에게 공고 가이드 및 전문가 작성법 내용 기반에 정보를 물어보고 완성도 높은 문서를 만들어보세요"
+    >
+        {props.children}
+    </Tooltip>
+        )
+    }
 
     return (
         <TableContainer component={Paper}>
@@ -35,6 +75,7 @@ export default function MyProjectTable(props) {
                     <TableRow>
                         <TableCell>프로젝트명</TableCell>
                         <TableCell align="right">상태</TableCell>
+                        <TableCell align="right">CHAT DOC</TableCell>
                         <TableCell align="right">최종 수정 날짜</TableCell>
                     </TableRow>
                 </TableHead>
@@ -55,6 +96,17 @@ export default function MyProjectTable(props) {
 
                                 </TableCell>
                                 <TableCell align="right">{project.projectState!=null?project.projectState:"첨삭중"}</TableCell>
+                                <TableCell align="right" >{project.chatDoc?
+                                    (
+                                        <CustomToolTip
+                                           children={  <img style={{cursor:"pointer"}} onClick={()=>{
+                                               alert("채팅창 띄우기")
+                                           }} width={"40px"} src="/favicon/android-icon-48x48.png" />}
+                                        />)
+                                    :
+                                    (<CustomToolTip children={(<img onClick={()=>{alert("공고문과 작성 가이드를 처리 중입니다. \n잠시만 기다려주세요.")}} style={{filter:"grayscale(100%)"}} width={"40px"} src="/favicon/android-icon-48x48.png" />)}
+                                    />)
+                                }</TableCell>
                                 <TableCell
                                     align="right">{moment(project.creationTime, 'YYYYMMDDHHmmss z').add(9, "h").fromNow()}</TableCell>
                             </TableRow>
